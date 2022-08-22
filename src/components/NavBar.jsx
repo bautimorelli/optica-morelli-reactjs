1032a47130381a067a23a499371be965f7f28485
Bautista Morelli <bautistamorelli@gmail.com>
2022-08-22 20:26:24 -0300

    Added NavBar Component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,137 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Badge from '@mui/material/Badge';
+const pages = ["Lentes de Sol", "Prescripciones", "Lentes de Contacto"];
+
+const ResponsiveAppBar = () => {
+	const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+	const handleOpenNavMenu = (event) => {
+		setAnchorElNav(event.currentTarget);
+	};
+
+	const handleCloseNavMenu = () => {
+		setAnchorElNav(null);
+	};
+
+	const styles = {
+		appbar: {
+			alignItems: "center",
+		},
+	};
+
+	return (
+		<AppBar position="static">
+			<Container maxWidth="xl">
+				<Toolbar disableGutters>
+					<Box
+						component="img"
+						sx={{
+							height: 70,
+							display: { xs: 'none', md: 'flex' },
+							mr: 1 
+						}}
+						alt="Optica Morelli logo"
+						src="images/logoOpticaTransparent.png"
+					/>
+
+					<Box
+						sx={{
+							flexGrow: 1,
+							display: { xs: "flex", md: "none" },
+						}}>
+						<IconButton
+							size="large"
+							aria-label="account of current user"
+							aria-controls="menu-appbar"
+							aria-haspopup="true"
+							onClick={handleOpenNavMenu}
+							color="inherit">
+							<MenuIcon />
+						</IconButton>
+						<Menu
+							id="menu-appbar"
+							anchorEl={anchorElNav}
+							anchorOrigin={{
+								vertical: "bottom",
+								horizontal: "left",
+							}}
+							keepMounted
+							transformOrigin={{
+								vertical: "top",
+								horizontal: "left",
+							}}
+							open={Boolean(anchorElNav)}
+							onClose={handleCloseNavMenu}
+							sx={{
+								display: { xs: "block", md: "none" },
+							}}>
+							{pages.map((page) => (
+								<MenuItem
+									key={page}
+									onClick={handleCloseNavMenu}>
+									<Typography textAlign="center">
+										{page}
+									</Typography>
+								</MenuItem>
+							))}
+						</Menu>
+					</Box>
+					<Box
+						component="img"
+						sx={{
+							height: 70,
+							display: { xs: 'flex', md: 'none' },
+							mr: 1 
+						}}
+						alt="Optica Morelli logo"
+						src="images/logoOpticaTransparent.png"
+					/>
+					<Box
+						sx={{
+							flexGrow: 1,
+							display: { xs: "none", md: "flex" },
+						}}>
+						{pages.map((page) => (
+							<Button
+								key={page}
+								onClick={handleCloseNavMenu}
+								sx={{
+									my: 2,
+									color: "white",
+									display: "block",
+								}}>
+								{page}
+							</Button>
+						))}
+					</Box>
+
+					<MenuItem>
+							<IconButton
+								size="large"
+								aria-label="2 items en el carrito"
+								color="inherit">
+								<Badge
+									badgeContent={2}
+									color="error">
+									<ShoppingCartIcon />
+								</Badge>
+							</IconButton>
+							<p>Carrito</p>
+						</MenuItem>
+				</Toolbar>
+			</Container>
+		</AppBar>
+	);
+};
+export default ResponsiveAppBar;
---
+++ src/components/itemListContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const itemListContainer = (props) => {
+    return (
+        <div>itemListContainer</div>
+    )
+}
+
+export default itemListContainer
