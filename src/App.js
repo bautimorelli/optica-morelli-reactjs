dc3a5bffb181064ab59c1abc8aaa6abe7b68b033
Bautista Morelli <bautistamorelli@gmail.com>
2022-09-06 10:16:43 -0300

    Added missing route from category to item

--- src/App.js
+++ src/App.js
@@ -4,22 +4,34 @@ import ItemDetailContainer from "./components/ItemDetailContainer";
 import ItemListContainer from "./components/ItemListContainer";
 import NavBar from "./components/NavBar";
 import theme from "./MuiTheme";
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
 	return (
 		<>
-		<BrowserRouter>
-			<ThemeProvider theme={theme}>
-				<NavBar />
-			</ThemeProvider>
-			<Routes>
-				<Route path="/" element={<ItemListContainer/>}/>
-				<Route path="/category/:categoryId" element={<ItemListContainer/>}/>
-				<Route path="/item/:id" element={<ItemDetailContainer/>}/>
-			</Routes>
-		</BrowserRouter>
+			<BrowserRouter>
+				<ThemeProvider theme={theme}>
+					<NavBar />
+				</ThemeProvider>
+				<Routes>
+					<Route
+						path="/"
+						element={<ItemListContainer />}
+					/>
+					<Route
+						path="/category/:categoryId"
+						element={<ItemListContainer />}
+					/>
+					<Route
+						path="/item/:id"
+						element={<ItemDetailContainer />}
+					/>
+					<Route
+						path="/category/:categoryId/item/:id"
+						element={<ItemDetailContainer />}
+					/>
+				</Routes>
+			</BrowserRouter>
 		</>
 	);
 }
