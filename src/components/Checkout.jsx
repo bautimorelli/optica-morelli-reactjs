78b69977ba7574aba2c726ceced3894f01d98131
Bautista Morelli <bautistamorelli@gmail.com>
2022-10-12 01:29:21 -0300

    Added react-material-ui-form-validator library

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,26 +1,18 @@
 import React, { useState } from "react";
 import { useCart } from "../context/CartContext";
 import { database } from "../firebase/firebase";
-import { Box, TextField, Typography } from "@mui/material";
-import { LoadingButton } from "@mui/lab";
+import { Box, Typography } from "@mui/material";
 import { addDoc, collection } from "firebase/firestore";
 import Toast from "./Toast";
+import CheckoutForm from "./CheckoutForm";
 
 const Checkout = () => {
 	const [id, setId] = useState("");
-	const [name, setName] = useState("");
-	const [email, setEmail] = useState("");
-	const [phone, setPhone] = useState("");
 	const [showId, setShowId] = useState(false);
-	const [loading, setLoading] = useState(false);
 	const [toast, setToast] = useState(false);
-
 	const { cart, totalPrice, clear } = useCart();
 
-	const checkoutForm = (e) => {
-		e.preventDefault();
-		setLoading(true);
-
+	const submitCheckout = (name, email, phone) => {
 		let order = {
 			buyer: {
 				name,
@@ -69,23 +61,23 @@ const Checkout = () => {
 					<Typography variant="h4">
 						Su compra fue realizada correctamente con el
 						identificador:
-						<Typography
-							variant="h4"
-							color={"#FF0000"}
-							sx={{
-								overflowWrap: "anywhere",
-							}}>
-							{id}
-						</Typography>
+						<div>
+							<Typography
+								variant="h4"
+								color={"#FF0000"}
+								sx={{
+									overflowWrap: "anywhere",
+								}}>
+								{id}
+							</Typography>
+						</div>
 					</Typography>
 				</Box>
 			) : (
 				<>
 					<Box
-						component="form"
-						onSubmit={checkoutForm}
 						sx={{
-							maxWidth: "25%",
+							maxWidth: "25vw",
 							mx: "auto",
 							my: "20vh",
 							minWidth: "250px",
@@ -93,42 +85,15 @@ const Checkout = () => {
 							padding: 3,
 							borderRadius: 4,
 						}}>
-						<Typography my={0}>Checkout</Typography>
-						<TextField
-							fullWidth
-							required
-							variant="standard"
-							margin="normal"
-							label="Nombre"
-							//inputProps={{pattern: "^([ \u00c0-\u01ffa-zA-Z'\-])+$"}}
-							onChange={(e) => setName(e.target.value)}
-						/>
-						<TextField
-							fullWidth
-							required
-							variant="standard"
-							margin="normal"
-							label="Email"
-							//inputProps={{pattern: "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"}}
-							onChange={(e) => setEmail(e.target.value)}
-						/>
-						<TextField
-							fullWidth
-							required
-							variant="standard"
-							margin="normal"
-							label="Celular"
-							//inputProps={{pattern: "^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$"}}
-							helperText="Tu numero sin el 15"
-							onChange={(e) => setPhone(e.target.value)}
-						/>
-						<LoadingButton
-							type="submit"
-							loading={loading}
-							variant="contained"
-							sx={{ mt: 3 }}>
-							Comprar
-						</LoadingButton>
+						<Typography
+							sx={{
+								textAlign: "center",
+								marginY: 2,
+								fontSize: "2rem",
+							}}>
+							Checkout
+						</Typography>
+						<CheckoutForm submitCheckout={submitCheckout} />
 					</Box>
 				</>
 			)}
---
+++ src/components/CheckoutForm.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import { LoadingButton } from "@mui/lab";
+import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
+import { Box } from "@mui/system";
+
+const CheckoutForm = ({submitCheckout}) => {
+	const [email, setEmail] = useState("");
+	const [name, setName] = useState("");
+	const [phone, setPhone] = useState("");
+	const [loading, setLoading] = useState(false);
+
+	const handleSubmit = (e) => {
+		setLoading(true);
+		submitCheckout(name, email, phone);
+	};
+
+	return (
+		<ValidatorForm onSubmit={handleSubmit}>
+			<Box sx={{display:"flex", flexDirection:"column"}}>
+				<TextValidator
+					label="Nombre"
+					name="nombre"
+					value={name}
+					validators={[
+						"required",
+						String.raw`matchRegexp:^([ \u00c0-\u01ffa-zA-Z'\-])+$`,
+					]}
+					errorMessages={[
+						"this field is required",
+						"El nombre no es valido",
+					]}
+					onChange={(e) => setName(e.target.value)}
+					sx={{ marginY: 1, width: "100%" }}
+				/>
+				<TextValidator
+					label="Email"
+					name="email"
+					value={email}
+					validators={["required", "isEmail"]}
+					errorMessages={[
+						"this field is required",
+						"El email no es valido",
+					]}
+					onChange={(e) => setEmail(e.target.value)}
+					sx={{ marginY: 1, width: "100%" }}
+				/>
+				<TextValidator
+					label="Celular"
+					name="celular"
+					value={phone}
+					validators={[
+						"required",
+						String.raw`matchRegexp:^\d{10,11}$`,
+					]}
+					errorMessages={[
+						"this field is required",
+						"El telefono no es valido",
+					]}
+					onChange={(e) => setPhone(e.target.value)}
+					sx={{ marginY: 1, width: "100%" }}
+				/>
+				<LoadingButton
+					type="submit"
+					loading={loading}
+					variant="contained"
+					sx={{ mt: 3, mx: "auto" }}>
+					Comprar
+				</LoadingButton>
+			</Box>
+		</ValidatorForm>
+	);
+};
+
+export default CheckoutForm;
